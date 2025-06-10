90f217b29d0dc154492bf03934cb310923d37974
Ferdi K <ferdi.kossmann@outlook.com>
2025-06-10 11:09:33 -0400

    remove code-gen log parsig

--- common/utils.py
+++
@@ -1,8 +0,0 @@
-import os
-import inspect
-
-def abs_path(rel_path):
-    frame = inspect.stack()[1]
-    caller_file = frame.filename
-    caller_dir = os.path.dirname(os.path.abspath(caller_file))
-    return os.path.abspath(os.path.join(caller_dir, rel_path))
